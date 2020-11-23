ecb4e2da8cddbab70de5599d45acc3e5ae80a57c
sumand4l <suman.mandal1020@gmail.com>
2020-11-23 23:11:22 +0530

    Q08 added

---
+++ DCCPCSTS3 Assignment-3/Q08.c
@@ -0,0 +1,12 @@
+/*Q08. Write a C program to print all alphabets from a to z. - using while loop.*/
+
+#include <stdio.h>
+int main(){
+    int i = 97;
+    printf("All Alphabets are:\n");
+    while(i <= 122){
+        printf("%c ",i);
+        i++;
+    }
+    return 0;
+}
