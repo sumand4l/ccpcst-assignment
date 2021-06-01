9864215c294fcc6671830c29585203a5518255fc
Suman Mondal <suman.mandal1020@gmail.com>
2021-06-01 20:59:21 +0530

    ds_array Q17 added

---
+++ data_structure/array/Q17.c
@@ -0,0 +1,33 @@
+/*Q17. Write a C Program to Delete the Specified number from an Array*/
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[50], n, t, i;
+    printf("Enter array length: ");
+    scanf("%d", &n);
+    printf("Enter array elements: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", arr + i);
+
+    printf("Enter element to delete: ");
+    scanf("%d", &t);
+
+    // Delete operation
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] == t)
+            break;
+    }
+    // Shift
+    for (; i < n; i++)
+        arr[i] = arr[i + 1];
+    n -= 1; // Reduce array length by 1.
+
+    printf("After delete: \n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
