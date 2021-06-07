0776fb6b080e429afc40b98713a7db999ce675de
Suman Mondal <suman.mandal1020@gmail.com>
2021-06-07 12:19:08 +0530

    [CODE FIX] Question 14

--- data_structure/array/Q14.c
+++ data_structure/array/Q14.c
@@ -1,26 +1,30 @@
-/*Q14. Write a C Program to multiply of Two Dimensional Arrays*/
+/*Q14. Write a C Program to Sum of Two 2-Dimensional Arrays*/
 
 #include <stdio.h>
 
 int main()
 {
-  int array[100][100], len_x, len_y, sum = 0;
-  printf("Array length: ");
-  scanf("%d%d", &len_x, &len_y);
-
-  for (int i = 0; i < len_x; i++)
-  {
-    for (int j = 0; j < len_y; j++)
-      scanf("%d", &array[i][j]);
+  int a[3][3], b[3][3], c[3][3], i, j;
+  printf("\nENTER VALUES FOR MATRIX A:\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      scanf("%d", &a[i][j]);
   }
-
-  for (int i = 0; i < len_x; i++)
-  {
-    for (int j = 0; j < len_y; j++)
-      sum += array[i][j];
+  printf("\nENTER VALUES FOR MATRIX B:\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      scanf("%d", &b[i][j]);
   }
-
-  printf("Sum: %d\n", sum);
-
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++)
+      c[i][j] = a[i][j] + b[i][j];
+  }
+  printf("\nTHE VALUES OF MATRIX C ARE:\n");
+  for (i = 0; i < 3; i++)
+  {
+    for (j = 0; j < 3; j++)
+      printf("%5d", c[i][j]);
+    printf("\n");
+  }
   return 0;
 }
