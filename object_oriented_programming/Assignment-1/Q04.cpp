dd1b5b7253ea4f7bc9edaf250e56f0449436181d
Suman Mondal <suman.mandal1020@gmail.com>
2021-06-08 18:30:59 +0530

    fibonacci series

--- object_oriented_programming/Assignment-1/Q04.cpp
+++ object_oriented_programming/Assignment-1/Q04.cpp
@@ -4,5 +4,16 @@
 using namespace std;
 
 int main () {
-    
+    int a = 0, b = 1, c, limit;
+    cout << "Enter Nth term to Print the Series: " << endl;
+    cin >> limit;
+    cout << "Fibonacci Series is: " << a << " " << b << " "; 
+    for (int i = 0; i <= limit; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+        cout << c << " ";
+    }
+    cout << endl;
+    return 0;
 }
