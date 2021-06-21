fbd70913535f6b2ff968f68f158146c2bf80d272
Suman Mondal <suman.mandal1020@gmail.com>
2021-06-22 02:18:38 +0530

    emp class

---
+++ object_oriented_programming/Assignment-2/Q04.cpp
@@ -0,0 +1,52 @@
+//Q04. Create Employee(Emp_Name, Id) class, create Manager Object 2(two), Scientist Object 1(one) and Labour Object 15(Fifteen). Write Member function Getdata and PrintData
+
+#include <iostream>
+using namespace std;
+//creating class
+class Employee {
+    private:
+        string Emp_Name;
+        int Id;
+    public:
+    void getData (int);
+    void printData ();
+};
+//getdata func defination
+void Employee :: getData (int iterator) {
+    cout << iterator <<"." << endl;
+    cout << "Enter Employee Id:: ";
+    cin >> Id;
+    cout << "Enter Employee Name:: ";
+    cin >> Emp_Name;
+}
+//printdata func defination
+void Employee :: printData () {
+    cout << Id << " -> " << Emp_Name << endl;
+}
+
+int main () {
+    Employee Manager[2], Scientist, Labour[15];
+
+    cout << "----Enter Manager Details----" << endl;
+    for (int i = 0; i < 2; i++)
+        Manager[i].getData (i+1);
+
+    cout << "----Enter Scientist Details----" << endl;
+    Scientist.getData (1);
+
+    cout << "----Enter Labour Details----" << endl;
+    for (int i = 0; i < 5; i++) 
+        Labour[i].getData (i+1);
+
+    cout << "<---Manager-->" << endl;
+    for (int i = 0; i < 2; i++)
+        Manager[i].printData ();
+
+    cout << "<---Scientist-->" << endl;
+    Scientist.printData ();
+    
+    cout << "<---Labour-->" << endl;
+    for (int i = 0; i < 5; i++)
+        Labour[i].printData ();
+    return 0;
+}
